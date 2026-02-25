5784f704cf1116b17a45f3f26dfcc737406b905c
irishi.bc <irishi@irishi.site>
2026-02-26 02:29:24 +0800

    add scripts
    新增角色数据和战斗基础脚本

---
+++ MTI/Assets/Scripts/Character/CharacterData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterData
+{
+    
+    public float Health;
+    public float MaxHealth;
+    public float Attack;
+    public float Defense;
+    public float Speed;
+    public float Critical;
+    public float AntiCrit;
+    public float Block;
+    public float AntiBlock;
+    
+}
